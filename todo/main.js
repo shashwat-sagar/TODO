5eb8c94ec0e7f97f830e3272c07e7adfc61e01dc
badass1901 <59369746+badass1901@users.noreply.github.com>
2020-09-23 20:11:31 +0530

    js

---
+++ todo/main.js
@@ -0,0 +1,7 @@
+const addButton = document.querySelector('.addButton');
+var inputValue = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+class item{
+    constructor(itemName{})
+}
