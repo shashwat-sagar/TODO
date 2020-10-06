5163f7f2c39f19280fcd7bd2f7905f7569a29842
badass1901 <59369746+badass1901@users.noreply.github.com>
2020-10-06 23:55:26 +0530

    Update main.js
    Added  NEW JS

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const addButton = document.querySelector('.addButton');
-var inputValue = document.querySelector('.input');
-const container = document.querySelector('.container');
+function addButton() {
+  console.log("Add Button is Clicked");
+  const elemClass = document.getElementsByClassName("container");
+  console.log(elemClass);
+  elemClass[0].classList.add("item1, item_input");
+  console.log("class added");
+}
 
-class item{
-    constructor(itemName{})
+function removeClicked() {
+  console.log("Remove Button is Clicked");
+  const elemClass = document.getElementsByClassName("container");
+  console.log(elemClass);
+  elemClass[0].classList.remove("item1");
+  console.log("class removed");
+}
+function editClicked() {
+  console.log("Edit Button Clicked");
+  const elemClass = document.getElementsByClassName("container");
+  elemClass[0].innerHTML =
+    '<input type="text" class="item_input" value="Sport" /><button class="editButton" onclick="editClicked()">Edit</button><button class="removeButton" onclick="removeClicked()">Remove</button>';
 }
