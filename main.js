1c8876b47006520952388c44d6d0a4e6da524c35
badass1901 <59369746+badass1901@users.noreply.github.com>
2020-10-07 21:16:26 +0530

    Revert "Revert "Added functionallity in EditButton""
    This reverts commit 067f4aa43761f2056ec8f1dbb2184352721ea26f.

---
+++ main.js
@@ -0,0 +1,40 @@
+const addButton = document.querySelector(".addButton");
+var inputValue = document.querySelector(".input");
+const container = document.querySelector(".container");
+
+class item {
+  constructor(itemName) {
+    this.createDiv(itemName);
+  }
+  createDiv(itemName) {
+    let input = document.createElement("input");
+    input.value = itemName;
+    input.disabled = true;
+    input.classList.add("item_input");
+
+    let itemBox = document.createElement("div");
+    itemBox.classList.add("item");
+
+    let editButton = document.createElement("button");
+    editButton.innerHTML = "EDIT";
+    editButton.classList.add("editButton");
+
+    let removeButton = document.createElement("button");
+    removeButton.innerHTML = "REMOVE";
+    removeButton.classList.add("removeButton");
+
+    container.appendChild(itemBox);
+
+    itemBox.appendChild(input);
+    itemBox.appendChild(editButton);
+    itemBox.appendChild(removeButton);
+
+    editButton.addEventListener("click", () => this.edit(input));
+  }
+  edit(input) {
+    input.disabled = !input.disabled;
+  }
+}
+
+new item("Driving Class");
+new item("Coding");
